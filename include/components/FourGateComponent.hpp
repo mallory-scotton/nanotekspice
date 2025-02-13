79e3d61c93dc3e827385f06ab9908d59c742664b
Hugo <hugo.cathelain@epitech.eu>
2025-02-13 20:14:01 +0100

    [ADD] link for x4gates, and function compute

--- include/components/FourGateComponent.hpp
+++ include/components/FourGateComponent.hpp
@@ -68,7 +68,23 @@ public:
     ///////////////////////////////////////////////////////////////////////////
     explicit FourGateComponent(const std::string& name)
         : AComponent(name, 14)
-    {}
+    {
+        m_gates[0].setlink(1, this, 1);
+        m_gates[0].setlink(2, this, 2);
+        m_gates[0].setlink(3, this, 3);
+
+        m_gates[1].setlink(1, this, 6);
+        m_gates[1].setlink(2, this, 5);
+        m_gates[1].setlink(3, this, 4);
+
+        m_gates[2].setlink(1, this, 8);
+        m_gates[2].setlink(2, this, 9);
+        m_gates[2].setlink(3, this, 10);
+
+        m_gates[3].setlink(1, this, 13);
+        m_gates[3].setlink(2, this, 12);
+        m_gates[3].setlink(3, this, 11);
+    }
 
 public:
     ///////////////////////////////////////////////////////////////////////////
@@ -91,6 +107,25 @@ public:
     Tristate compute(size_t pin) override
     {
         (void)pin;
+        size_t min = 0;
+        if (pin < 1 || pin > 14)
+            throw std::out_of_range("Invalid pin for x4Gate component");
+        if (pin < 4)
+            return m_gates[0]->compute(pin)
+        if (pin < 7) {
+            min = pin == 4 ? 1 : pin;
+            min = pin == 6 ? 5 : pin;
+            return m_gates[1]->compute(pin - min)
+        }
+        if (pin == 7)
+            return (Tristate::Undefinded);
+        if (pin < 11)
+            return m_gates[2]->compute(pin - 7)
+        if (pin < 14) {
+            min = pin == 11 ? 8 : pin;
+            min = pin == 13 ? 12 : pin;
+            return m_gates[3]->compute(pin - min)
+        }
         return (Tristate::Undefined);
     }
 };
@@ -101,5 +136,21 @@ using C4011Component = FourGateComponent<NandGateComponent>;
 using C4030Component = FourGateComponent<XorGateComponent>;
 using C4071Component = FourGateComponent<OrGateComponent>;
 using C4081Component = FourGateComponent<AndGateComponent>;
+
+// link pin 1 to pin 1 of mgate[0]
+// link pin 2 to pin 2 of mgate[0]
+// link pin 3 to pin 3 of mgate[0]
+
+// link pin 4 to pin 3 of mgate[0]
+// link pin 5 to pin 2 of mgate[0]
+// link pin 6 to pin 1 of mgate[0]
+
+// link pin 8 to pin 1 of mgate[0]
+// link pin 9 to pin 2 of mgate[0]
+// link pin 10 to pin 3 of mgate[0]
+
+// link pin 11 to pin 3 of mgate[0]
+// link pin 12 to pin 2 of mgate[0]
+// link pin 13 to pin 1 of mgate[0]
 
 } // namespace nts
