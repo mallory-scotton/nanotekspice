13e7fdb219c42158edf3e3bb0a63191622317bc9
Hugo <hugo.cathelain@epitech.eu>
2025-02-17 16:46:44 +0100

    [ADD] component C4008 4 bits adder

--- source/components/C4008Component.cpp
+++ source/components/C4008Component.cpp
@@ -37,7 +37,7 @@ namespace nts
 
 ///////////////////////////////////////////////////////////////////////////////
 C4008Component::C4008Component(const std::string& name)
-    : AComponent(name, 14)
+    : AComponent(name, 16)
 {}
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -46,12 +46,61 @@ void C4008Component::simulate(size_t tick)
     (void)tick;
 }
 
+Tristate C4008Component::getInputPinValue(size_t pin)
+{
+    if (!m_links[pin - 1].first)
+        return (Tristate::Undefined);
+    return (m_links[pin - 1].first->compute(m_links[pin - 1].second));
+}
+
+Tristate C4008Component::intToTristate(int n)
+{
+    return (n == 0 ? Tristate::False : Tristate::True);
+}
+
+Tristate C4008Component::calculus(size_t pin)
+{
+    if (getInputPinValue(1) == Tristate::Undefined ||
+        getInputPinValue(2) == Tristate::Undefined ||
+        getInputPinValue(3) == Tristate::Undefined ||
+        getInputPinValue(4) == Tristate::Undefined ||
+        getInputPinValue(5) == Tristate::Undefined ||
+        getInputPinValue(6) == Tristate::Undefined ||
+        getInputPinValue(7) == Tristate::Undefined ||
+        getInputPinValue(9) == Tristate::Undefined ||
+        getInputPinValue(15) == Tristate::Undefined
+    )
+        return (Tristate::Undefined);
+    int A = getInputPinValue(7) << 0 |
+        getInputPinValue(5) << 1 |
+        getInputPinValue(3) << 2 |
+        getInputPinValue(1) << 3;
+    int B = getInputPinValue(6) << 0 |
+        getInputPinValue(4) << 1 |
+        getInputPinValue(5) << 2 |
+        getInputPinValue(15) << 3;
+    int carry_in = getInputPinValue(9);
+    int sum = A + B + carry_in;
+
+    if (pin == 10)
+        return intToTristate((sum >> 0) & 1);
+    if (pin == 11)
+        return intToTristate((sum >> 1) & 1);
+    if (pin == 12)
+        return intToTristate((sum >> 2) & 1);
+    if (pin == 13)
+        return intToTristate((sum >> 3) & 1);
+    return intToTristate((sum >> 4) & 1);
+}
+
 ///////////////////////////////////////////////////////////////////////////////
 Tristate C4008Component::compute(size_t pin)
 {
-    if (pin != 1)
+    if (pin < 1 || pin > 15 || pin == 8)
         throw std::out_of_range("Invalid pin for 4008 component");
-    return (Tristate::False);
+    if (pin == 10 || pin == 11 || pin == 12 || pin == 13 || pin == 14)
+        return calculus(pin);
+    return (getInputPinValue(pin));
 }
 
 } // namespace nts
