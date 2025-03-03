cc43e4a49f521f9f91a3ae491b33b23a265ba860
Hugo <hugo.cathelain@epitech.eu>
2025-03-03 12:56:05 +0100

    [FIX] repair the flip flop and the pins link in the components raaah

--- Components/C4013.cpp
+++ Components/C4013.cpp
@@ -19,22 +19,22 @@ C4013::C4013(const std::string& name)
     }
     , m_initialized(false)
 {
+    m_pins[2] = Pin(Pin::Type::INPUT);
     m_pins[3] = Pin(Pin::Type::INPUT);
-    m_pins[5] = Pin(Pin::Type::INPUT);
-    m_pins[6] = Pin(Pin::Type::INPUT);
     m_pins[4] = Pin(Pin::Type::INPUT);
+    m_pins[5] = Pin(Pin::Type::INPUT);
+    m_pins[0] = Pin(Pin::Type::OUTPUT);
     m_pins[1] = Pin(Pin::Type::OUTPUT);
-    m_pins[2] = Pin(Pin::Type::OUTPUT);
 
-    m_pins[11] = Pin(Pin::Type::INPUT);
-    m_pins[9] = Pin(Pin::Type::INPUT);
+    m_pins[7] = Pin(Pin::Type::INPUT);
     m_pins[8] = Pin(Pin::Type::INPUT);
+    m_pins[9] = Pin(Pin::Type::INPUT);
     m_pins[10] = Pin(Pin::Type::INPUT);
-    m_pins[13] = Pin(Pin::Type::OUTPUT);
+    m_pins[11] = Pin(Pin::Type::OUTPUT);
     m_pins[12] = Pin(Pin::Type::OUTPUT);
 
-    m_pins[0] = Pin(Pin::Type::ELECTRICAL);
-    m_pins[7] = Pin(Pin::Type::ELECTRICAL);
+    m_pins[6] = Pin(Pin::Type::ELECTRICAL);
+    m_pins[13] = Pin(Pin::Type::ELECTRICAL);
 }
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -42,15 +42,15 @@ void C4013::initializeLinks(void)
 {
     if (m_initialized)
         return;
-    m_flipflops[0].setLink(0, shared_from_this(), 3);
-    m_flipflops[0].setLink(1, shared_from_this(), 5);
-    m_flipflops[0].setLink(2, shared_from_this(), 6);
-    m_flipflops[0].setLink(3, shared_from_this(), 4);
+    m_flipflops[0].setLink(0, shared_from_this(), 2);
+    m_flipflops[0].setLink(1, shared_from_this(), 4);
+    m_flipflops[0].setLink(2, shared_from_this(), 5);
+    m_flipflops[0].setLink(3, shared_from_this(), 3);
 
-    m_flipflops[1].setLink(0, shared_from_this(), 11);
-    m_flipflops[1].setLink(1, shared_from_this(), 9);
-    m_flipflops[1].setLink(2, shared_from_this(), 8);
-    m_flipflops[1].setLink(3, shared_from_this(), 10);
+    m_flipflops[1].setLink(0, shared_from_this(), 10);
+    m_flipflops[1].setLink(1, shared_from_this(), 8);
+    m_flipflops[1].setLink(2, shared_from_this(), 7);
+    m_flipflops[1].setLink(3, shared_from_this(), 9);
 
     m_initialized = true;
 }
@@ -62,25 +62,25 @@ Tristate C4013::compute(size_t pin)
         initializeLinks();
 
     switch (pin) {
-        case 0:
-        case 7:
+        case 6:
+        case 13:
             return (Tristate::Undefined);
         case 1:
             return (m_flipflops[0].compute(4));
-        case 2:
+        case 0:
             return (m_flipflops[0].compute(5));
-        case 13:
+        case 11:
             return (m_flipflops[1].compute(4));
         case 12:
             return (m_flipflops[1].compute(5));
+        case 2:
         case 3:
         case 4:
         case 5:
-        case 6:
+        case 7:
         case 8:
         case 9:
         case 10:
-        case 11:
             return (getInputState(pin));
         default:
             throw OutOfRangePinException();
--- Components/C4017.cpp
+++ Components/C4017.cpp
@@ -21,25 +21,26 @@ C4017::C4017(const std::string& name)
         std::make_shared<nts::Sequencials::FlipFlop>(name + "_5"),
     }
     , m_initialized(false)
+    , last_ff4(Tristate::True)
 {
+    m_pins[12] = Pin(Pin::Type::INPUT);
     m_pins[13] = Pin(Pin::Type::INPUT);
     m_pins[14] = Pin(Pin::Type::INPUT);
-    m_pins[15] = Pin(Pin::Type::INPUT);
 
+    m_pins[0]  = Pin(Pin::Type::OUTPUT);
     m_pins[1]  = Pin(Pin::Type::OUTPUT);
     m_pins[2]  = Pin(Pin::Type::OUTPUT);
     m_pins[3]  = Pin(Pin::Type::OUTPUT);
     m_pins[4]  = Pin(Pin::Type::OUTPUT);
     m_pins[5]  = Pin(Pin::Type::OUTPUT);
     m_pins[6]  = Pin(Pin::Type::OUTPUT);
-    m_pins[7]  = Pin(Pin::Type::OUTPUT);
+    m_pins[8] = Pin(Pin::Type::OUTPUT);
     m_pins[9]  = Pin(Pin::Type::OUTPUT);
     m_pins[10] = Pin(Pin::Type::OUTPUT);
     m_pins[11] = Pin(Pin::Type::OUTPUT);
-    m_pins[12] = Pin(Pin::Type::OUTPUT);
 
-    m_pins[0]  = Pin(Pin::Type::ELECTRICAL);
-    m_pins[8]  = Pin(Pin::Type::ELECTRICAL);
+    m_pins[7]  = Pin(Pin::Type::ELECTRICAL);
+    m_pins[15]  = Pin(Pin::Type::ELECTRICAL);
 }
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -49,55 +50,56 @@ void C4017::initializeLinks(void)
         return;
     auto self = shared_from_this();
 
-    m_flip_flops[0]->setLink(0, self, 14);
+    m_flip_flops[0]->setLink(0, self, 13);
     m_flip_flops[0]->setLink(1, m_flip_flops[4], 5);
-    m_flip_flops[0]->setLink(3, self, 15);
+    m_flip_flops[0]->setLink(2, self, 14);
 
-    m_flip_flops[1]->setLink(0, self, 14);
+    m_flip_flops[1]->setLink(0, self, 13);
     m_flip_flops[1]->setLink(1, m_flip_flops[0], 4);
-    m_flip_flops[1]->setLink(3, self, 15);
+    m_flip_flops[1]->setLink(3, self, 14);
 
-    m_flip_flops[2]->setLink(0, self, 14);
+    m_flip_flops[2]->setLink(0, self, 13);
     m_flip_flops[2]->setLink(1, m_flip_flops[1], 4);
-    m_flip_flops[2]->setLink(3, self, 15);
+    m_flip_flops[2]->setLink(3, self, 14);
 
-    m_flip_flops[3]->setLink(0, self, 14);
+    m_flip_flops[3]->setLink(0, self, 13);
     m_flip_flops[3]->setLink(1, m_flip_flops[2], 4);
-    m_flip_flops[3]->setLink(3, self, 15);
+    m_flip_flops[3]->setLink(3, self, 14);
 
-    m_flip_flops[4]->setLink(0, self, 14);
+    m_flip_flops[4]->setLink(0, self, 13);
     m_flip_flops[4]->setLink(1, m_flip_flops[3], 4);
-    m_flip_flops[4]->setLink(3, self, 15);
+    m_flip_flops[4]->setLink(3, self, 14);
 
     m_initialized = true;
 }
 
 Tristate C4017::decoder(size_t pin)
 {
-    Tristate bit_1 = m_flip_flops[0]->compute(5);
-    Tristate bit_2 = m_flip_flops[1]->compute(5);
-    Tristate bit_3 = m_flip_flops[2]->compute(5);
-    Tristate bit_4 = m_flip_flops[3]->compute(5);
-    Tristate bit_5 = m_flip_flops[4]->compute(5);
-    if (pin == 1 && (bit_1 && bit_2 && bit_3 && bit_4 && bit_5)) // count 5
+    bool bit_1 = m_flip_flops[0]->compute(4) == Tristate::True ? True : False;
+    bool bit_2 = m_flip_flops[1]->compute(4) == Tristate::True ? True : False;
+    bool bit_3 = m_flip_flops[2]->compute(4) == Tristate::True ? True : False;
+    bool bit_4 = m_flip_flops[3]->compute(4) == Tristate::True ? True : False;
+    bool bit_5 = m_flip_flops[4]->compute(4) == Tristate::True ? True : False;
+
+    if (pin == 0 && (bit_1 && bit_2 && bit_3 && bit_4 && bit_5)) // count 5
         return Tristate::True;
-    if (pin == 2 && (bit_1 && !bit_2 && !bit_3 && !bit_4 && !bit_5)) // count 1
+    if (pin == 1 && (bit_1 && !bit_2 && !bit_3 && !bit_4 && !bit_5)) // count 1
         return Tristate::True;
-    if (pin == 3 && (!bit_1 && !bit_2 && !bit_3 && !bit_4 && !bit_5)) // count 0
+    if (pin == 2 && (!bit_1 && !bit_2 && !bit_3 && !bit_4 && !bit_5)) // count 0
         return Tristate::True;
-    if (pin == 4 && (bit_1 && bit_2 && !bit_3 && !bit_4 && !bit_5)) // count 2
+    if (pin == 3 && (bit_1 && bit_2 && !bit_3 && !bit_4 && !bit_5)) // count 2
         return Tristate::True;
-    if (pin == 5 && (!bit_1 && bit_2 && bit_3 && bit_4 && bit_5)) // count 6
+    if (pin == 4 && (!bit_1 && bit_2 && bit_3 && bit_4 && bit_5)) // count 6
         return Tristate::True;
-    if (pin == 6 && (!bit_1 && !bit_2 && bit_3 && bit_4 && bit_5)) // count 7
+    if (pin == 5 && (!bit_1 && !bit_2 && bit_3 && bit_4 && bit_5)) // count 7
         return Tristate::True;
-    if (pin == 7 && (bit_1 && bit_2 && bit_3 && !bit_4 && !bit_5)) // count 3
+    if (pin == 6 && (bit_1 && bit_2 && bit_3 && !bit_4 && !bit_5)) // count 3
         return Tristate::True;
-    if (pin == 9 && (!bit_1 && !bit_2 && !bit_3 && bit_4 && bit_5)) // count 8
+    if (pin == 8 && (!bit_1 && !bit_2 && !bit_3 && bit_4 && bit_5)) // count 8
         return Tristate::True;
-    if (pin == 10 && (bit_1 && bit_2 && bit_3 && bit_4 && !bit_5)) // count 4
+    if (pin == 9 && (bit_1 && bit_2 && bit_3 && bit_4 && !bit_5)) // count 4
         return Tristate::True;
-    if (pin == 11 && (!bit_1 && !bit_2 && !bit_3 && !bit_4 && bit_5)) // count 9
+    if (pin == 10 && (!bit_1 && !bit_2 && !bit_3 && !bit_4 && bit_5)) // count 9
         return Tristate::True;
     return Tristate::False;
 }
@@ -109,24 +111,24 @@ Tristate C4017::compute(size_t pin)
         initializeLinks();
 
     switch (pin) {
-        case 0:
-        case 8:
-            return Tristate::Undefined;
-        case 3:
-        case 2:
-        case 4:
         case 7:
-        case 10:
+        case 15:
+            return Tristate::Undefined;
+        case 0:
         case 1:
+        case 2:
+        case 3:
+        case 4:
         case 5:
         case 6:
+        case 8:
         case 9:
+        case 10:
         case 11:
+            return (decoder(pin));
         case 12:
-            return decoder(pin);
-        case 14:
         case 13:
-        case 15:
+        case 14:
             return getInputState(pin);
         default:
             throw OutOfRangePinException();
@@ -139,6 +141,12 @@ void C4017::simulate(size_t tick)
     if (m_tick == tick)
         return;
     AComponent::simulate(tick);
+
+    m_flip_flops[4]->simulate(tick);
+    m_flip_flops[3]->simulate(tick);
+    m_flip_flops[2]->simulate(tick);
+    m_flip_flops[1]->simulate(tick);
+    m_flip_flops[0]->simulate(tick);
 
     for (size_t pin : {1, 2, 3, 4, 5, 6, 7, 9, 10, 11, 12}) {
         propagateOutput(pin, compute(pin));
