3ec5f98f8f957729f1d3b3461e5fc6153a0c3739
mallory-scotton <mallory.scotton@epitech.eu>
2025-03-05 17:13:07 +0100

    fix: fixing C4094

--- Components/C4094.cpp
+++ Components/C4094.cpp
@@ -23,7 +23,7 @@ C4094::C4094(const std::string& name)
     m_pins[1] = Pin(Pin::Type::INPUT);
     m_pins[2] = Pin(Pin::Type::INPUT);
     m_pins[14] = Pin(Pin::Type::INPUT);
-    
+
     m_pins[3] = Pin(Pin::Type::OUTPUT);
     m_pins[4] = Pin(Pin::Type::OUTPUT);
     m_pins[5] = Pin(Pin::Type::OUTPUT);
@@ -34,7 +34,7 @@ C4094::C4094(const std::string& name)
     m_pins[10] = Pin(Pin::Type::OUTPUT);
     m_pins[8] = Pin(Pin::Type::OUTPUT);
     m_pins[9] = Pin(Pin::Type::OUTPUT);
-    
+
     m_pins[7] = Pin(Pin::Type::ELECTRICAL);
     m_pins[15] = Pin(Pin::Type::ELECTRICAL);
 }
@@ -46,10 +46,10 @@ Tristate C4094::compute(size_t pin)
 
     if (pin == 7 || pin == 15) {
         return Tristate::Undefined;
-    }    
+    }
     if (pin == 0 || pin == 1 || pin == 2 || pin == 14) {
         return getInputState(pin);
-    }    
+    }
     if (outputEnable == Tristate::False) {
         if (pin != 8 && pin != 9) {
             return Tristate::Undefined;
@@ -57,26 +57,25 @@ Tristate C4094::compute(size_t pin)
     }
     switch (pin) {
         case 3:  // Q1
-            return ((m_storageRegister >> 0) & 0x01) ? Tristate::True : Tristate::False;
+            return ((m_storageRegister >> 0) & 0x01) ? Tristate::False : Tristate::True;
         case 4:  // Q2
-            return ((m_storageRegister >> 1) & 0x01) ? Tristate::True : Tristate::False;
+            return ((m_storageRegister >> 1) & 0x01) ? Tristate::False : Tristate::True;
         case 5:  // Q3
-            return ((m_storageRegister >> 2) & 0x01) ? Tristate::True : Tristate::False;
+            return ((m_storageRegister >> 2) & 0x01) ? Tristate::False : Tristate::True;
         case 6:  // Q4
-            return ((m_storageRegister >> 3) & 0x01) ? Tristate::True : Tristate::False;
+            return ((m_storageRegister >> 3) & 0x01) ? Tristate::False : Tristate::True;
         case 13: // Q5
-            return ((m_storageRegister >> 4) & 0x01) ? Tristate::True : Tristate::False;
+            return ((m_storageRegister >> 4) & 0x01) ? Tristate::False : Tristate::True;
         case 12: // Q6
-            return ((m_storageRegister >> 5) & 0x01) ? Tristate::True : Tristate::False;
+            return ((m_storageRegister >> 5) & 0x01) ? Tristate::False : Tristate::True;
         case 11: // Q7
-            return ((m_storageRegister >> 6) & 0x01) ? Tristate::True : Tristate::False;
+            return ((m_storageRegister >> 6) & 0x01) ? Tristate::False : Tristate::True;
         case 10: // Q8
-            return ((m_storageRegister >> 7) & 0x01) ? Tristate::True : Tristate::False;
+            return ((m_storageRegister >> 7) & 0x01) ? Tristate::False : Tristate::True;
         case 8:  // QS (Serial output)
-            return m_serialOut;
+            return (m_serialOut);
         case 9:  // QS' (Complement of serial output)
-            return (m_serialOut == Tristate::True) ? Tristate::False : 
-                  (m_serialOut == Tristate::False) ? Tristate::True : Tristate::Undefined;
+            return (~m_serialOut);
         default:
             throw OutOfRangePinException();
     }
@@ -92,34 +91,34 @@ void C4094::simulate(size_t tick)
     Tristate strobe = getInputState(0);
     Tristate data = getInputState(1);
     Tristate clock = getInputState(2);
-    
+
     // Serial out is always the last bit of the shift register
     m_serialOut = ((m_shiftRegister >> 7) & 0x01) ? Tristate::True : Tristate::False;
-    
+
     // On rising edge of clock, shift data through the register
     if (m_lastClock == Tristate::False && clock == Tristate::True) {
         // Shift everything one position
         m_shiftRegister = m_shiftRegister << 1;
-        
+
         // Insert new data bit at position 0
         if (data == Tristate::True) {
             m_shiftRegister |= 0x01;
         } else {
             m_shiftRegister &= ~0x01;
         }
-        
+
         // Make sure we stay within 8 bits
         m_shiftRegister &= 0xFF;
     }
-    
+
     // On rising edge of strobe, transfer shift register to storage register
     if (m_lastStrobe == Tristate::False && strobe == Tristate::True) {
         m_storageRegister = m_shiftRegister;
     }
-    
+
     m_lastClock = clock;
     m_lastStrobe = strobe;
-    
+
     // Propagate all outputs
     for (size_t pin : {3, 4, 5, 6, 8, 9, 10, 11, 12, 13}) {
         propagateOutput(pin, compute(pin));
