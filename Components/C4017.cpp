9f15b3afcab09661b82f5dbe7209dc7f54264398
Hugo <hugo.cathelain@epitech.eu>
2025-03-08 10:37:49 +0100

    [REM] flip flops from 4017 and change it for a basic counter like 4040

--- Components/C4017.cpp
+++ Components/C4017.cpp
@@ -13,15 +13,7 @@ namespace nts::Components
 ///////////////////////////////////////////////////////////////////////////////
 C4017::C4017(const std::string& name)
     : AComponent(name, 16)
-    , m_flip_flops{
-        std::make_shared<nts::Sequencials::FlipFlop>(name + "_1"),
-        std::make_shared<nts::Sequencials::FlipFlop>(name + "_2"),
-        std::make_shared<nts::Sequencials::FlipFlop>(name + "_3"),
-        std::make_shared<nts::Sequencials::FlipFlop>(name + "_4"),
-        std::make_shared<nts::Sequencials::FlipFlop>(name + "_5"),
-    }
-    , m_initialized(false)
-    , last_ff4(Tristate::True)
+    , m_count(0)
 {
     m_pins[12] = Pin(Pin::Type::INPUT, "CP0");
     m_pins[13] = Pin(Pin::Type::INPUT, "CP1");
@@ -43,63 +35,27 @@ C4017::C4017(const std::string& name)
     m_pins[15] = Pin(Pin::Type::ELECTRICAL, "QDD");
 }
 
-///////////////////////////////////////////////////////////////////////////////
-void C4017::initializeLinks(void)
-{
-    if (m_initialized)
-        return;
-    auto self = shared_from_this();
-
-    m_flip_flops[0]->setLink(0, self, 13);
-    m_flip_flops[0]->setLink(1, m_flip_flops[4], 4);
-    m_flip_flops[0]->setLink(2, self, 14);
-
-    m_flip_flops[1]->setLink(0, self, 13);
-    m_flip_flops[1]->setLink(1, m_flip_flops[0], 5);
-    m_flip_flops[1]->setLink(3, self, 14);
-
-    m_flip_flops[2]->setLink(0, self, 13);
-    m_flip_flops[2]->setLink(1, m_flip_flops[1], 5);
-    m_flip_flops[2]->setLink(3, self, 14);
-
-    m_flip_flops[3]->setLink(0, self, 13);
-    m_flip_flops[3]->setLink(1, m_flip_flops[2], 5);
-    m_flip_flops[3]->setLink(3, self, 14);
-
-    m_flip_flops[4]->setLink(0, self, 13);
-    m_flip_flops[4]->setLink(1, m_flip_flops[3], 5);
-    m_flip_flops[4]->setLink(3, self, 14);
-
-    m_initialized = true;
-}
-
 Tristate C4017::decoder(size_t pin)
 {
-    bool bit_1 = (m_flip_flops[0]->compute(4) == Tristate::True);
-    bool bit_2 = (m_flip_flops[1]->compute(4) == Tristate::True);
-    bool bit_3 = (m_flip_flops[2]->compute(4) == Tristate::True);
-    bool bit_4 = (m_flip_flops[3]->compute(4) == Tristate::True);
-    bool bit_5 = (m_flip_flops[4]->compute(4) == Tristate::True);
-
-    if ((pin == 0 || pin == 11) && (bit_1 && bit_2 && bit_3 && bit_4 && bit_5)) // count 5
+    if ((pin == 0 || pin == 11) && m_count == 5) // count 5
         return (Tristate::True);
-    if ((pin == 1 || pin == 11) && (bit_1 && !bit_2 && !bit_3 && !bit_4 && !bit_5)) // count 1
+    if ((pin == 1 || pin == 11) && m_count == 1) // count 1
         return (Tristate::True);
-    if ((pin == 2 || pin == 11) && (!bit_1 && !bit_2 && !bit_3 && !bit_4 && !bit_5)) // count 0
+    if ((pin == 2 || pin == 11) && m_count == 0) // count 0
         return (Tristate::True);
-    if (pin == 3 && (bit_1 && bit_2 && !bit_3 && !bit_4 && !bit_5)) // count 2
+    if (pin == 3 && m_count == 2) // count 2
         return (Tristate::True);
-    if (pin == 4 && (!bit_1 && bit_2 && bit_3 && bit_4 && bit_5)) // count 6
+    if (pin == 4 && m_count == 6) // count 6
         return (Tristate::True);
-    if (pin == 5 && (!bit_1 && !bit_2 && bit_3 && bit_4 && bit_5)) // count 7
+    if (pin == 5 && m_count == 7) // count 7
         return (Tristate::True);
-    if ((pin == 6 || pin == 11) && (bit_1 && bit_2 && bit_3 && !bit_4 && !bit_5)) // count 3
+    if ((pin == 6 || pin == 11) && m_count == 3) // count 3
         return (Tristate::True);
-    if (pin == 8 && (!bit_1 && !bit_2 && !bit_3 && bit_4 && bit_5)) // count 8
+    if (pin == 8 && m_count == 8) // count 8
         return (Tristate::True);
-    if ((pin == 9 || pin == 11) && (bit_1 && bit_2 && bit_3 && bit_4 && !bit_5)) // count 4
+    if ((pin == 9 || pin == 11) && m_count == 4) // count 4
         return (Tristate::True);
-    if (pin == 10 && (!bit_1 && !bit_2 && !bit_3 && !bit_4 && bit_5)) // count 9
+    if (pin == 10 && m_count == 9) // count 9
         return (Tristate::True);
     return Tristate::False;
 }
@@ -107,9 +63,6 @@ Tristate C4017::decoder(size_t pin)
 ///////////////////////////////////////////////////////////////////////////////
 Tristate C4017::compute(size_t pin)
 {
-    if (!m_initialized)
-        initializeLinks();
-
     switch (pin) {
         case 7:
         case 15:
@@ -141,12 +94,19 @@ void C4017::simulate(size_t tick)
     if (m_tick == tick)
         return;
     AComponent::simulate(tick);
+    Tristate cp0 = getInputState(12);
+    Tristate cp1 = getInputState(13);
+    Tristate mr = getInputState(14);
 
-    m_flip_flops[4]->simulate(tick);
-    m_flip_flops[3]->simulate(tick);
-    m_flip_flops[2]->simulate(tick);
-    m_flip_flops[1]->simulate(tick);
-    m_flip_flops[0]->simulate(tick);
+    if (mr == Tristate::True) {
+        m_count = 0;
+    } else if (cp0 == Tristate::True && cp1 == Tristate::False && m_lastCp0 != Tristate::True) {
+        m_count += m_count < 9 ? 1 : -9;
+    } else if (cp0 == Tristate::True && cp1 == Tristate::True && m_lastCp1 != Tristate::True) {
+        m_count += m_count < 9 ? 1 : -9;
+    }
+    m_lastCp0 = cp0;
+    m_lastCp1 = cp1;
 
     for (size_t pin : {0, 1, 2, 3, 4, 5, 6, 8, 9, 10, 11}) {
         propagateOutput(pin, compute(pin));
